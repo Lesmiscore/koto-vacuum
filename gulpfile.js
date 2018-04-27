58af7638719cd995f1f80e1cdc8a07109c2bdbe3
nao20010128nao <nao20010128@gmail.com>
2018-04-27 11:36:43 +0900

    gulpで圧縮

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require("gulp-uglify");
+//const babel = require("gulp-babel");
+const compiler = require('google-closure-compiler-js').gulp();
+
+gulp.task('js', function(){
+  return browserify('./index.js')
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(compiler({
+    compilationLevel: 'SIMPLE',
+    warningLevel: 'VERBOSE',
+    outputWrapper: '(function(){\n%output%\n}).call(this)',
+    jsOutputFile: 'closure-mini.js',
+    createSourceMap: true,
+  }))
+  .pipe(uglify('mini.js'))
+  .on("error",console.log)
+  .pipe(gulp.dest('./dist/'));
+});
